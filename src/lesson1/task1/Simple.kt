feadfb099ff8a90fea6c08e9cc7828a6c54601bb
Aon <alexandr.mal.38@gmail.com>
2019-09-24 17:39:07 +0300

    flex

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -88,7 +88,10 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(
+    sqr(x2 - x1) +
+            sqr(y2 - y1)
+)
 
 /**
  * Простая
@@ -99,8 +102,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(s
 fun thirdDigit(number: Int): Int {
     val a1 = number / 10
     val a2 = a1 / 10
-    val a3 = a2 % 10
-    return a3
+    return a2 % 10
 
 
 }
