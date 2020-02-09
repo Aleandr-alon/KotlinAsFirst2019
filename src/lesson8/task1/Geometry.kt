ad6d4bb511b69885853d6f392fc705e8bc84e762
Aon <alexandr.mal.38@gmail.com>
2020-02-10 02:31:37 +0300

    flex

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -151,7 +151,7 @@ class Line private constructor(val b: Double, val angle: Double) {
  * Построить прямую по отрезку
  */
 fun lineBySegment(s: Segment): Line =
-    Line(s.begin, (2 * PI + (atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x)))) % PI)
+    Line(s.begin, (PI + (atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x)))) % PI)
 
 /**
  * Средняя
